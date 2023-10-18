e06f61c645a860e6a84c7dfa1a1770833d9b95ac
miguelrm81 <miguelrm81@users.noreply.github.com>
2023-10-18 16:00:26 +0200

    actualizacion ticket3-1

--- app.py
+++ app.py
@@ -17,3 +17,10 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 st.set_option('deprecation.showPyplotGlobalUse', False)
+
+# Developer 1 - Parte 3
+'''
+Cargamos los datos
+'''
+train_df = pd.read_csv('data/train.csv')
+test_df = pd.read_csv('data/test.csv')
