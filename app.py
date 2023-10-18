9bfd6a2d53c9ddd54e7512c6034b0063d6e93de1
Agui <gonzalezvalleag@gmail.com>
2023-10-18 17:05:35 +0200

    part 3 dev 2

--- app.py
+++ app.py
@@ -17,3 +17,9 @@ test_df['Embarked'].fillna(test_df['Embarked'].mode()[0], inplace=True)
 train_df['CabinBool'] = (train_df['Cabin'].notnull().astype('int'))
 test_df['CabinBool'] = (test_df['Cabin'].notnull().astype('int')) train_df = pd.concat([train_df, pd.get_dummies(train_df['Embarked'], prefix='Embarked')], axis=0)
 test_df = pd.concat([test_df, pd.get_dummies(test_df['Embarked'], prefix='Embarked')], axis=0)
+
+# Developer 2 - Parte 4
+''' Hacemos Featuring Engineering '''
+train_df = pd.concat([train_df, pd.get_dummies(train_df['Embarked'], prefix='Embarked')], axis=1)
+test_df = pd.concat([test_df, pd.get_dummies(test_df['Embarked'], prefix='Embarked')], axis=1)
+test_df['Fare'].fillna(test_df['Fare'].dropna().mean(), inplace=True)
