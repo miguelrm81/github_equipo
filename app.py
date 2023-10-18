f8b398f0764abb16d47411f41f330c32a5ea33bc
miguelrm81 <145917465+miguelrm81@users.noreply.github.com>
2023-10-18 20:22:09 +0200

    actualizacion epic-branch-4

--- app.py
+++ app.py
@@ -90,6 +90,41 @@ dead_age = train_df[train_df['Survived']==0]['AgeGroup'].value_counts(sort=False
 age_df = pd.DataFrame([survived_age,dead_age],index=['Survived','Dead'])
 age_df.plot(kind='bar', stacked=True)
 plt.xlabel('Age Group')
+
+'''
+Visualizamos la variable Clase
+'''
+survived_class = train_df[train_df['Survived']==1]['Pclass'].value_counts(sort=False) dead_class = train_df[train_df['Survived']==0]['Pclass'].value_counts(sort=False) class_df = pd.DataFrame([survived_class,dead_class],index=['Survived','Dead']) class_df.plot(kind='bar', stacked=True)
+plt.xlabel('Class')
+plt.ylabel('Number of passengers')
+plt.title('Survival rate by class')
+st.pyplot()
+
+
+'''
+Visualizamos la variable Sexo '''
+survived_sex = train_df[train_df['Survived']==1]['Sex'].value_counts(sort=False) dead_sex = train_df[train_df['Survived']==0]['Sex'].value_counts(sort=False) sex_df = pd.DataFrame([survived_sex,dead_sex],index=['Survived','Dead']) sex_df.plot(kind='bar', stacked=True)
+plt.xlabel('Sex')
+plt.ylabel('Number of passengers') plt.title('Survival rate by sex') st.pyplot()
+
+
+'''
+Visualizamos la variable Embarked '''
+survived_embark = train_df[train_df['Survived']==1]['Embarked'].value_counts(sort=False)
+dead_embark = train_df[train_df['Survived']==0]['Embarked'].value_counts(sort=False) embark_df = pd.DataFrame([survived_embark,dead_embark],index=['Survived','Dead']) embark_df.plot(kind='bar', stacked=True)
+plt.xlabel('Embarked')
+plt.ylabel('Number of passengers')
+plt.title('Survival rate by port of embarkation')
+st.pyplot()
+
+
+'''
+Visualizamos la importancia de las variables
+'''
+survived_embark = train_df[train_df['Survived']==1]['Embarked'].value_counts(sort=True)
+dead_embark = train_df[train_df['Survived']==0]['Embarked'].value_counts(sort=False) st.write('Visualización del modelo y datos')
+st.write('Importancia de características en el modelo')
+st.bar_chart(importances)
 plt.ylabel('Number of passengers')
 plt.title('Distribution of passengers by age and survival')
 st.pyplot()
