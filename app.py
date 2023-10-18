9aacec8c4b31bcd635b3fee74b7619ab749cca60
miguelrm81 <145917465+miguelrm81@users.noreply.github.com>
2023-10-18 20:20:15 +0200

    actualizacion epic-branch-3

--- app.py
+++ app.py
@@ -51,3 +51,46 @@ train_df = pd.concat([train_df, pd.get_dummies(train_df['Embarked'], prefix='Emb
 test_df = pd.concat([test_df, pd.get_dummies(test_df['Embarked'], prefix='Embarked')], axis=1)
 test_df['Fare'].fillna(test_df['Fare'].dropna().mean(), inplace=True)
 
+# Developer 3 - Parte 1
+'''
+Generamos Variables dependientes e independientes
+'''
+variables=['Pclass', 'Age', 'Sex', 'SibSp', 'Parch', 'Fare', 'CabinBool', 'Embarked_C', 
+'Embarked_S', 'Embarked_Q']
+X = train_df[variables]
+y = train_df['Survived']
+# Developer 3 - Parte 2
+'''
+Creamos el modelo
+'''
+X = train_df[['Pclass', 'Age', 'Sex', 'SibSp', 'Parch', 'Fare', 'CabinBool', 'Embarked_C', 
+'Embarked_S', 'Embarked_Q]]
+y = train_df[[Survived']]
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=0)
+model = RandomForestClassifier(n_estimators=100, random_state=0)
+model.fit(X_train, y_train)
+# Developer 3 - Parte 3
+'''Hacemos predicciones'''
+model.fit(X_train, y_val)
+y_pred = model.predict(X_val)
+accuracy = accuracy_score(y_val, y_pred)
+importances = 
+pd.DataFrame({'feature':X_train.columns,'importance':np.round(model.feature_importan
+ces_,3)})
+importances = 
+importances.sort_values('importance',ascending=False).set_index('feature')
+# Developer 3 - Parte 4
+'''
+Visualizamos la variable Age
+'''
+bins = [0, 10, 20, 30, 40, 50, 60, 70, 80]
+train_df['AgeGroup'] = pd.cut(train_df['Age'], bins)
+survived_age = train_df[train_df['Survived']==1]['AgeGroup'].value_counts(sort=False)
+dead_age = train_df[train_df['Survived']==0]['AgeGroup'].value_counts(sort=False)
+age_df = pd.DataFrame([survived_age,dead_age],index=['Survived','Dead'])
+age_df.plot(kind='bar', stacked=True)
+plt.xlabel('Age Group')
+plt.ylabel('Number of passengers')
+plt.title('Distribution of passengers by age and survival')
+st.pyplot()
+
