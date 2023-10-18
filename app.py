2ae31e37ca533bd3dd2b815a2ad8948b9687e34c
miguelrm81 <miguelrm81@users.noreply.github.com>
2023-10-18 15:51:34 +0200

    commit ticket 2

--- app.py
+++ app.py
@@ -6,3 +6,14 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Developer 1 - Parte 2
+'''
+Importamos librerías
+'''
+import pandas
+import numpy
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+st.set_option('deprecation.showPyplotGlobalUse', False)
